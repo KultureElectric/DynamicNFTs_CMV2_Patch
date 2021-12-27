9f010b2c72d8a67ac70a90197f6f44821830e139
Ruben Schaller <rubenusshd@gmail.com>
2021-12-27 17:47:08 +0200

    patch

---
+++ js/packages/cli/src/helpers/cache.ts
@@ -0,0 +1,47 @@
+import fs from 'fs';
+import path from 'path';
+import { CACHE_PATH } from './constants';
+
+export function cachePath(
+  env: string,
+  cacheName: string,
+  cPath: string = CACHE_PATH,
+  legacy: boolean = false,
+) {
+  const filename = `${env}-${cacheName}`;
+  return path.join(cPath, legacy ? filename : `${filename}.json`);
+}
+
+export function loadCache(
+  cacheName: string,
+  env: string,
+  cPath: string = CACHE_PATH,
+  legacy: boolean = false,
+) {
+  const path = cachePath(env, cacheName, cPath, legacy);
+  console.log(path);
+  
+
+  if (!fs.existsSync(path)) {
+    if (!legacy) {
+      return loadCache(cacheName, env, cPath, true);
+    }
+    return undefined;
+  }
+
+  return JSON.parse(fs.readFileSync(path).toString());
+}
+
+export function saveCache(
+  cacheName: string,
+  env: string,
+  cacheContent,
+  cPath: string = CACHE_PATH,
+) {
+  cacheContent.env = env;
+  cacheContent.cacheName = cacheName;
+  fs.writeFileSync(
+    cachePath(env, cacheName, cPath),
+    JSON.stringify(cacheContent),
+  );
+}
